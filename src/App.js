52984fc783457b244b3a46ba7c78e25cb3ac821a
jabed366 <jabedhasan231@gmail.com>
2022-05-02 18:26:27 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import './_assets/css/bundle.css';
+import './_assets/css/style.css';
 
 function App() {
   return (
     <>
+    <Header></Header>
+    <Footer/>
     </>
   );
 }
