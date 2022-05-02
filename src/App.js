2ba1101952bed322591d68054a93ad4b84ca6646
jabed366 <jabedhasan231@gmail.com>
2022-05-02 21:34:59 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+import Home from './Components/Pages/Home';
+import Shop from './Components/Pages/Shop';
+import SingleProduct from './Components/Pages/SingleProduct';
 import './_assets/css/bundle.css';
 import './_assets/css/style.css';
 
 function App() {
   return (
+
+   
     <>
-    <Header></Header>
-    <Footer/>
+     <BrowserRouter>
+     <Header/>
+
+     <Routes>
+
+       <Route path='/' element={<Home/>}/>
+       <Route path='/shop' element={<Shop/>}/>
+       <Route path='/shop/:name' element={<SingleProduct/>}/>
+     </Routes>
+     <Footer/>
+    </BrowserRouter>
+    
+   
+    {/* <Shop/> */}
+    
     </>
   );
 }
